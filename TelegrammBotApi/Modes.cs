d612c647b94d873088ba2d8760d3a942b3e81090
srgo2 <srgo2@DESKTOP-F2FP0OU>
2020-05-19 01:29:38 +0400

    Метод для прокси

--- TelegrammBotApi/Modes.cs
+++ TelegrammBotApi/Modes.cs
@@ -1,16 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net;
 
 namespace TelegrammBotApi
 {
     internal class Modes
     {
+        WebClient client;
+
+        static string url {get;set;}
         public Modes(string token)
         {
             string baseUrl = "https://api.telegram.org/bot";
-            string url = baseUrl + token + "/";
+            string Url = baseUrl + token + "/";
 
+            url = Url;
+        }//public Modes
+
+
+        void Init()
+        {
+            client = new WebClient();
+            InitProxy();
+        
         }
-    }
-}
+
+        /// <summary>Устанавливаем прокси</summary>
+        private void InitProxy()
+        {
+            //установка прокси
+            WebProxy proxy = new WebProxy("216.158.228.233", 7384);
+            proxy.Credentials = new NetworkCredential("iusa3000334", "Kpoe0eSXDv");
+            client.Proxy = proxy;
+        }
+
+
+
+    }//class Modes
+}//namespace TelegrammBotApi
--- TelegrammBotApi/Program.cs
+++ TelegrammBotApi/Program.cs
@@ -8,11 +8,11 @@ namespace TelegrammBotApi
     class Program
     {
 
-        WebClient client;
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Старт шаблона");
+            Console.WriteLine("Старт шаблона!");
 
             string token = "392463607:AAH2MyFC5A0PUPD0JFfwYILTErdtn45Civ8";
 
