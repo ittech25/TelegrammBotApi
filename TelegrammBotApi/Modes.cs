4115f77a88088b7bbda2aea88ed0e30a586a68a2
srgo2 <srgo2@DESKTOP-F2FP0OU>
2020-05-19 01:12:06 +0400

    Добавьте файлы проекта.

---
+++ TelegrammBotApi/Modes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegrammBotApi
+{
+    internal class Modes
+    {
+        public Modes(string token)
+        {
+            string baseUrl = "https://api.telegram.org/bot";
+            string url = baseUrl + token + "/";
+
+        }
+    }
+}
---
+++ TelegrammBotApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+namespace TelegrammBotApi
+{
+    class Program
+    {
+
+        WebClient client;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Старт шаблона!");
+
+            string token = "392463607:AAH2MyFC5A0PUPD0JFfwYILTErdtn45Civ8";
+
+            Modes mod = new Modes(token);
+            
+
+
+        }
+
+
+
+
+    }
+}
