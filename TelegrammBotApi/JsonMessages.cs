62f686b98ce12dd24482c14d0b517a8c0fcf5b8c
srgo2 <srgo2@DESKTOP-F2FP0OU>
2020-05-19 22:05:40 +0400

    Делаем асинхронные методы - добавляем Json

---
+++ TelegrammBotApi/JsonMessages.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegrammBotApi
+{
+    public class JsonMessages
+    {
+        public class Finish
+        {
+            /// <summary>
+            /// Статус запроса
+            /// </summary>
+            public bool ok { get; set; }
+            /// <summary>
+            /// Результат ответа в виде массива
+            /// </summary>
+            public Result[] result { get; set; }
+        }
+
+        /// <summary>
+        /// Класс отвечающий за все входящие сообщения
+        /// </summary>
+        public class Result
+        {
+            /// <summary>
+            /// Порядковый номер входящего сообщения
+            /// </summary>
+            public long update_id { get; set; }
+            /// <summary>
+            /// класс входящих сообщения и все что с ними связано
+            /// </summary>
+            public Message message { get; set; }
+
+            public CallbackQuery callback_query { get; set; }
+        }
+
+
+        public class CallbackQuery
+        {
+            public string id { get; set; }
+            public From from { get; set; }
+            public Message message { get; set; }
+            public string chat_instance { get; set; }
+            public string data { get; set; }
+        }
+
+        public class Message
+        {
+            public int message_id { get; set; }
+            public From from { get; set; }
+            /// <summary>
+            /// класс отвечающий за Чат
+            /// </summary>
+            public Chat chat { get; set; }
+            public int date { get; set; }
+            public string text { get; set; }
+            public Entity[] entities { get; set; }
+
+            public ReplyMarkup reply_markup { get; set; }
+        }
+
+
+        public class ReplyMarkup
+        {
+            public List<List<object>> inline_keyboard { get; set; }
+        }
+        public class From
+        {
+            public int id { get; set; }
+            public bool is_bot { get; set; }
+            public string first_name { get; set; }
+            public string username { get; set; }
+            public string language_code { get; set; }
+        }
+
+        public class Chat
+        {
+            /// <summary>
+            /// ID чата с которого пишут
+            /// </summary>
+            public int id { get; set; }
+            /// <summary>
+            /// Фамилия того кто пишет
+            /// </summary>
+            public string first_name { get; set; }
+            /// <summary>
+            /// Имя того кто пишет
+            /// </summary>
+            public string username { get; set; }
+            /// <summary>
+            /// тип сообщения
+            /// </summary>
+            public string type { get; set; }
+        }
+
+        public class Entity
+        {
+            public int offset { get; set; }
+            public int length { get; set; }
+            public string type { get; set; }
+        }
+
+    }//class JsonMessages
+}
--- TelegrammBotApi/Modes.cs
+++ TelegrammBotApi/Modes.cs
@@ -1,36 +1,115 @@
 ﻿using System;
 using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Leaf.xNet;
+using System.Xml;
+using Newtonsoft.Json;
 
 namespace TelegrammBotApi
 {
     internal class Modes
     {
-        WebClient client;
+        
+        HttpClient client;
+        //Порядковый номер сообщения
+        long countUpdateId = 0;
 
         static string url {get;set;}
+        public string result { get; set; }
         public Modes(string token)
         {
             string baseUrl = "https://api.telegram.org/bot";
             string Url = baseUrl + token + "/";
 
             url = Url;
+
+            client = InitProxy(client);
+
+            while (true)
+            {
+                GetUpdates();
+
+            }
+            
+
+
+
         }//public Modes
 
-
-        void Init()
+        //Получаем сообщения
+        async void  GetUpdates()
         {
-            client = new WebClient();
-            InitProxy();
-        
+            //Запрос получение новых сообщений, offset - делает сообщение прочитанным
+            var res = client.GetStringAsync(url + $"getUpdates?offset={countUpdateId.ToString()}").Result;
+                Console.WriteLine(res);
+                Console.WriteLine();
+
+
+            JsonMessages.Finish MessageNew = JsonConvert.DeserializeObject<JsonMessages.Finish>(res.ToString());
+
+            if (!MessageNew.ok || MessageNew.result.Length == 0) return;
+
+            //Получаем порядковый номер сообщения
+            countUpdateId = MessageNew.result[0].update_id;
+
+            countUpdateId++;
+
+            await Task.Delay(3000);
+
         }
 
+
+
         /// <summary>Устанавливаем прокси</summary>
-        private void InitProxy()
+        private HttpClient InitProxy(HttpClient Client)
         {
-            //установка прокси
-            WebProxy proxy = new WebProxy("216.158.228.233", 7384);
-            proxy.Credentials = new NetworkCredential("iusa3000334", "Kpoe0eSXDv");
-            client.Proxy = proxy;
+
+            string proxyHost = "216.158.228.233";
+            string proxyPort = "7384";
+            string proxyUserName = "iusa3000334";
+            string proxyPassword = "Kpoe0eSXDv";
+
+            // First create a proxy object
+            var proxy = new WebProxy
+            {
+                Address = new Uri($"http://{proxyHost}:{proxyPort}"),
+                BypassProxyOnLocal = false,
+                UseDefaultCredentials = false,
+
+                // *** These creds are given to the proxy server, not the web server ***
+                Credentials = new NetworkCredential(
+                    userName: proxyUserName,
+                    password: proxyPassword)
+            };
+
+            // Now create a client handler which uses that proxy
+            var httpClientHandler = new HttpClientHandler
+            {
+                Proxy = proxy,
+            };
+
+            // Omit this part if you don't need to authenticate with the web server:
+            /*
+            if (needServerAuthentication)
+            {
+                httpClientHandler.PreAuthenticate = true;
+                httpClientHandler.UseDefaultCredentials = false;
+
+                // *** These creds are given to the web server, not the proxy server ***
+                httpClientHandler.Credentials = new NetworkCredential(
+                    userName: serverUserName,
+                    password: serverPassword);
+            }
+            */
+
+            // Finally, create the HTTP client object
+            Client =  new HttpClient(handler: httpClientHandler, disposeHandler: true);
+
+            return Client;
+
+
         }
 
 
