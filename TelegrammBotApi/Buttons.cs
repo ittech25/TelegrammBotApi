e3fde57934460e1463135451d605a0363199990b
srgo2 <srgo2@DESKTOP-F2FP0OU>
2020-05-23 03:51:57 +0400

    Inline Menu

---
+++ TelegrammBotApi/Buttons.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegrammBotApi
+{
+    class Buttons
+    {
+
+        /// <summary>
+        /// Этот класс представляет встроенную клавиатуру, которая появляется под соответствующим сообщением.
+        /// </summary>
+        public class InlineKeyboardMarkup
+        {
+            /// <summary>Массив строк, каждая из которых является массивом объектов InlineKeyboardButton</summary>
+            public List<List<InlineKeyboardButton>> inline_keyboard { get; set; }
+
+
+            /// <summary>
+            /// Конструктор, Массив массивов с InlineKeyboardButton
+            /// </summary>
+            /// <param name="inline_keyboard"> Массив кнопок</param>
+            public InlineKeyboardMarkup(List<List<InlineKeyboardButton>> inline_keyboard)
+            {
+                this.inline_keyboard = inline_keyboard;
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Этот объект представляет одну кнопку встроенной клавиатуры
+        /// </summary>
+        public class InlineKeyboardButton
+        {
+            /// <summary>Текст на кнопке</summary>
+            public string text { get; set; }
+            /// <summary>URL, который откроется при нажатии на кнопку</summary>
+            public string url { get; set; }
+            /// <summary>Данные, которые будут отправлены в callback_query при нажатии на кнопку</summary>
+            public string callback_data { get; set; }
+
+
+
+
+            /// <summary>
+            /// Создаем кнопки
+            /// </summary>
+            /// <param name="text">Текст кнопки</param>
+            /// <param name="url">URL адрес кнопки</param>
+            /// <param name="callback_data">Данные, которые будут отправлены в callback_query при нажатии на кнопку</param>
+            public InlineKeyboardButton(string text, string url, string callback_data)
+
+            {
+                this.text = text;
+                this.url = url;
+                this.callback_data = callback_data;
+                
+                
+                if (this.callback_data == "")
+                {
+                    this.callback_data = url;
+                    if (url == "")
+                        this.callback_data = text;
+                }
+
+
+            }
+
+            /// <summary>
+            /// Создаем кнопки
+            /// </summary>
+            /// <param name="text">Текст кнопки</param>
+            /// <param name="callback_data">Данные, которые будут отправлены в callback_query при нажатии на кнопку</param>
+            public InlineKeyboardButton(string text, string callback_data)
+            {
+                url = "";
+                this.text = text;
+                this.callback_data = callback_data;
+                if (callback_data == "") this.callback_data = text;
+
+            }
+
+        }
+
+    }
+}
--- TelegrammBotApi/Messages.cs
+++ TelegrammBotApi/Messages.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using TelegrammBotApi;
+using static TelegrammBotApi.Buttons;
 
 namespace TelegrammBotApi
 {
@@ -22,6 +26,10 @@ namespace TelegrammBotApi
                 case @"/start":
                     answer = "Добро пожаловать! Вы запустили чат бот. \r\nЕсли не заете с чего начить введиде: /menu ";
                     break;
+
+                case @"/menu":
+                    InlineMenu(ChatId);
+                    return;
 
                 default:
                     answer = $"Вы мне написали:\r\n{Message}\r\nЯ не знаю что ответить, воспользуйтесь командой: /menu";
@@ -50,5 +58,55 @@ namespace TelegrammBotApi
             var res = await Settings.Client.PostAsync(url, content);
 
         }
+
+
+
+
+
+
+        //List<List<InlineKeyboardButton>> keybort = new List<List<InlineKeyboardButton>>();
+        //создаем линии из кнопок
+        //List<InlineKeyboardButton> Line2 = new List<InlineKeyboardButton>();
+
+        /// <summary>
+        /// Создаем Inline меню
+        /// </summary>
+        /// <param name="ChatId"></param>
+        string InlineMenu(string ChatId)
+        {
+
+            
+            //создаем кнопку 1
+            InlineKeyboardButton key1 = new InlineKeyboardButton("кнопка 1", "ya.ru","адрес перенаправления");
+            //создаем кнопку 2
+            InlineKeyboardButton key2 = new InlineKeyboardButton("кнопка 2", "Отобразится данное сообщение");
+
+            //Создаем 1 линию(строку) из кнопок
+            List<InlineKeyboardButton> keyBtn = new List<InlineKeyboardButton>()
+            {
+                key1,
+                key2
+            };
+            
+
+            //Создаем все кнопки
+            List<List<InlineKeyboardButton>> keybort = new List<List<InlineKeyboardButton>>()
+            {
+                keyBtn,
+                keyBtn,
+                keyBtn,
+            };
+
+            InlineKeyboardMarkup allBtn = new InlineKeyboardMarkup(keybort);
+
+
+            //необходимо сериализовать класс в JSON
+            string replyMarkup = JsonConvert.SerializeObject(allBtn);
+            //отправка сообщения 
+            sendMessage(ChatId, "INLINE Меню", replyMarkup);
+
+            return replyMarkup;
+        }
+
     }
 }
